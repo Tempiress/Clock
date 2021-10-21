112db55f36554fde16a55fe948b3882a258ed2b5
Varlamov Danil <danil.varlamov.03@mail.ru>
2021-10-22 01:20:55 +0300

    Clear code

--- Clock.pas
+++ Clock.pas
@@ -1,57 +1,45 @@
 ﻿uses graphWPF, WPFObjects;
 
-procedure clock();
+procedure clock;
 begin
-  var x0,y0, x1,y1,x2,y2,x3,y3: integer;
-  var currTime := System.DateTime.Now;
-    pen.Width := 3;
-   Circle(250,250,170);
-   //line(250,250,250,80);
+  var x1,y1,x2,y2,x3,y3: integer;
+  var currTime := System.DateTime.Now; // местное время
+ 
+// рисование круга 
+pen.Width := 3;
+Circle(250,250,170);
    
-//font.Style := Bold; 
+   
+ //Расстановка цифер
 textout(235,90,'12');
-
 textout(243,380,'6');
-
 textout(385,240,'3');
-
 textout(105,240,'9');
-
 textout(115,165,'10');
-
 textout(160,110,'11');
-
 textout(320,110,'1');
-
 textout(370,165,'2');
-
 textout(370,310,'4');
-
 textout(315,365,'5');
-
 textout(160,365,'7');
-
 textout(115,310,'4');
 
 
-// Секундная Стрелка
-x1:=250+Round(120*sin(Pi*currtime.Second/30)); //координаты конечной точки секундной стрелки
+//координаты конечной точки секундной стрелки
+x1:=250+Round(120 * sin(Pi * currtime.Second / 30)); //координаты конечной точки секундной стрелки
+y1:=250-Round(120 * cos(Pi * currtime.Second / 30));
+line(250,250,x1,y1);
 
-y1:=250-Round(120*cos(Pi*currtime.Second/30));
 
-line(250,250,x1,y1);//Секундная стрелка
-
-// Минутная Стрелка
-
-x2:=250+Round(100*sin(Pi*currtime.Minute/30)); //координаты конечной точки минутной стрелки
-
-y2:=250-Round(100*cos(Pi*currtime.Minute/30));
+//координаты конечной точки минутной стрелки
+x2 := 250 + Round(100*sin(Pi * currtime.Minute / 30)); 
+y2 := 250 - Round(100*cos(Pi * currtime.Minute / 30));
 line(250,250,x2,y2);
 
-//Часовая Стрелка
 
-x3 := 250 + Round(60*sin(Pi * currtime.Hour/30));
-y3 := 250 - Round(60*cos(Pi * currtime.Hour/30)); 
+//координаты конечной точки часовой стрелки
+x3 := 250 + Round(60*sin(Pi * currtime.Hour / 30));
+y3 := 250 - Round(60*cos(Pi * currtime.Hour / 30)); 
 line(250,250,x3,y3);
 
 end;
@@ -62,6 +50,6 @@ begin
   begin   
   clock;
   sleep(1000);
-  window.Clear
+  window.Clear;
   end;
 end.
